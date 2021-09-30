e8c78921f6a865ad2dd50cc38f4a374c165df4fe
Lian Paul <lianpaulmolo08@gmail.com>
2021-09-30 17:35:17 +0800

    adding styles and function to the todo list item

--- js/app.js
+++ js/app.js
@@ -4,22 +4,48 @@ const todoList = document.querySelector('.todo-list');
 
 form.addEventListener('submit', addTodo);
 
-function addTodo(e) {
-  e.preventDefault();
-
+function addTodo(event) {
+  event.preventDefault();
+  
   const value = input.value;
+  input.value = '';
+
   const item = document.createElement('li');
   item.classList.add('todo-item');
+  
+  // if value is not empty add list item
   if(value) {
-    item.innerHTML = `<div class="checkbox" aria-label="checkbox"></div>
+    item.innerHTML = `<button class="check-item checkbox" aria-label="checkbox"></button>
     <p class="todo-text">${value}</p>
     <button class="remove-btn">
       <img src="./images/icon-cross.svg" alt="">
     </button>`;
-
     todoList.append(item)
   };
 
-  input.value = ''
+  // adding check button
+  const checkItem = item.querySelector('.check-item');
+  const todoText = item.querySelector('.todo-text');
+
+  checkItem.addEventListener('click', () => {
+    checkItem.classList.toggle('toggleCheck')
+    todoText.classList.toggle('item-complete');
+  })
+  todoText.addEventListener('click', () => {
+    checkItem.classList.toggle('toggleCheck');
+    todoText.classList.toggle('item-complete');
+  })
+
+  // adding remove button
+  const removeItem = item.querySelector('.remove-btn');
+  removeItem.addEventListener('click', () => {
+    removeItem.parentElement.remove();
+  })
 }
 
+
+
+// saving in local storage
+// localStorage.setItem('names', JSON.stringify(['desirie','lian']))
+// const loversNames = JSON.parse(localStorage.getItem('names'));
+// console.log(loversNames);
